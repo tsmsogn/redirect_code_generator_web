e3fc27f96ed8f3a82252c0f4c27247f985801d10
tsmsogn <tsmsogn@gmail.com>
2019-02-28 09:57:20 +0900

    tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,8 +2,20 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
 describe 'My Sinatra Application' do
-  it 'should allow accessing the home page' do
-    get '/'
-    expect(last_response).to be_ok
+
+  describe '/' do
+    context 'GET' do
+      it 'works' do
+        get '/'
+        expect(last_response).to be_ok
+      end
+    end
+
+    context 'POST' do
+      it 'works' do
+        post '/', :input => '/old.html /new.html'
+        expect(last_response).to be_ok
+      end
+    end
   end
 end
