12ef6432f84e6abec10f61d295d4c237207f4c86
tsmsogn <tsmsogn@gmail.com>
2019-02-28 09:38:45 +0900

    Fix string literals

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,8 +1,8 @@
 # spec/app_spec.rb
 require File.expand_path '../spec_helper.rb', __FILE__
 
-describe "My Sinatra Application" do
-  it "should allow accessing the home page" do
+describe 'My Sinatra Application' do
+  it 'should allow accessing the home page' do
     get '/'
     expect(last_response).to be_ok
   end
